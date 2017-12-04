1e6e17bca4af5552829d64641d1fbb1bc67d65a0
NicolaiVBonderup <nvbonderup@gmail.com>
2017-12-04 10:28:59 +0100

    Added TODO

--- main.py
+++ main.py
@@ -12,3 +12,4 @@ server.activate_server()
 # Add transaction values, to allow for properly determining when to mine
 # Add block analysis, when new block is announced to network, check if any transactions are actively being mined, if they are, stop mining and remove transactions from the received block
 # Might require threading, maybe start with just finishing mining, check if there's any new blocks invalidating afterwards.
+# Make tests to guide development. Easier to figure out what to do when we have a checklist of things that the system should be able to do.
